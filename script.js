0dd7ab0c0f5596753d3e705cba94825d8c3b61f1
sahlct <mhdsahlct@gmail.com>
2024-01-18 17:17:53 +0530

    completed

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let title, message, date;
 let entris = [];
+let entrissec = [];
 let entry;
 let newDiv, new1, new2;
 let count = 0;
 let divId;
 let mainid;
 let selectedIndex, cards;
+let selectvalue;
+let enterDate;
+
+
+function checkOption() {
+    selectvalue = document.getElementById("select_box").value
+    console.log(selectvalue);
+    if (selectvalue == 1) {
+        document.getElementById("filterdate").style.display = "none"
+        document.getElementById("filterdate").value = ''
+        rearrangeDivs()
+    } else {
+        document.getElementById("filterdate").style.display = "flex";
+    }
+}
 
 document.getElementById("myform").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -14,42 +30,13 @@ document.getElementById("myform").addEventListener("submit", function (event) {
     date = document.getElementById("date").value;
     document.getElementById("empty").style.display = "none";
     event.target.reset();
-    while (true) {
-        count++
+    count++
 
-        entry = { title, message, date };
-        entris.push(entry);
-        console.log(entry);
-
-        newDiv = document.createElement("div");
-        newDiv.id = "creatediv";
-        newDiv.className = 'card_div'
-
-        newDiv.innerHTML = `
-            <div id="title-div">
-                <h3>${entry.title}</h3>
-                <h6>${entry.message}</h6>
-                <h6>${entry.date}</h6>
-            </div>
-            <div style="flex-grow: 1;">
-
-            </div>
-            <div id="button-div">
-                <button type="button" id="delete" class="btn btn-success" onclick="datadelete(${count})">Delete</button>
-                <button type="button" id="edit" class="btn btn-primary" data-bs-toggle="modal"
-                data-bs-target="#editmodal" onclick="editindex(${count})">Edit</button>
-                <button type="button" id="mark" class="btn btn-danger" onclick="read(${count})">Mark us complete</button>
-            </div>
-    `
-
-        document.getElementById("container-fluid").appendChild(newDiv);
-        document.getElementById("container-fluid").style.flexDirection = "column"
-        document.getElementById("container-fluid").style.alignItems = "center"
-        document.getElementById("container-fluid").style.justifyContent = "flex-start";
-        console.log(entris);
-        console.log(count - 1);
-        return
-    }
+    entry = { title, message, date };
+    entris.push(entry);
+    entrissec.push(entry);
+    console.log(entry);
+    rearrangeDivs()
 });
 
 
@@ -67,6 +54,9 @@ document.getElementById("myform-second").addEventListener("submit", function (ev
     entris[selectedIndex - 1].title = document.getElementById("title-second").value;
     entris[selectedIndex - 1].message = document.getElementById("message-second").value;
     entris[selectedIndex - 1].date = document.getElementById("date-second").value;
+    entrissec[selectedIndex - 1].title = document.getElementById("title-second").value;
+    entrissec[selectedIndex - 1].message = document.getElementById("message-second").value;
+    entrissec[selectedIndex - 1].date = document.getElementById("date-second").value;
 
     cards = document.getElementsByClassName('card_div');
     console.log(cards);
@@ -83,7 +73,7 @@ document.getElementById("myform-second").addEventListener("submit", function (ev
             <button type="button" id="delete" class="btn btn-success" onclick="datadelete(${selectedIndex})">Delete</button>
             <button type="button" id="edit" class="btn btn-primary" data-bs-toggle="modal"
                 data-bs-target="#editmodal" onclick="editindex(${selectedIndex})">Edit</button>
-            <button type="button" id="mark" class="btn btn-danger" onclick="read(${selectedIndex})">Mark us complete</button>
+            <button type="button" id="mark" class="btn btn-danger" onclick="read(${selectedIndex})">Mark as complete</button>
         </div>
     `;
     console.log("selectedindex ", selectedIndex);
@@ -100,7 +90,7 @@ function read(count) {
     const readercard = reader[selectedIndex - 1]
     let classcheck = "mainread"
     console.log("count is", count - 1);
-    let j=0
+    let j = 0
     console.log(entris[j].title);
     console.log(entris[j].message);
     if (readercard.classList.contains(classcheck)) {
@@ -114,28 +104,29 @@ function datadelete(datacount) {
     selectedIndex = datacount;
     const deleter = document.getElementsByClassName('card_div');
     entris.splice(selectedIndex - 1, 1)
-    while (true) {
-        const deletercard = deleter[0]
-        deletercard.remove();
-        console.log("deleter is ", deleter);
-        console.log("entris is ", entris);
-        console.log("entris length",entris.length);
-        removeAllCardDivs()
-        break;
-    }
+    const deletercard = deleter[0]
+    deletercard.remove();
+    console.log("deleter is ", deleter);
+    console.log("entris is ", entris);
+    console.log("entris length", entris.length);
     rearrangeDivs()
 }
 
 function rearrangeDivs() {
-    const container = document.getElementById("container-fluid");
-    container.innerHTML = ''; 
-    count = 0;
-    while( count < entris.length) {
-        const newDiv = document.createElement("div");
-        newDiv.id = "creatediv";
-        newDiv.className = 'card_div'
+    removeAllCardDivs()
+    enterDate = document.getElementById("filterdate").value
 
-        newDiv.innerHTML = `
+    const container = document.getElementById("container-fluid");
+    container.innerHTML = '';
+    count = 0;
+    while (count < entris.length) {
+        if (enterDate == entris[count].date || enterDate === '') {
+
+            const newDiv = document.createElement("div");
+            newDiv.id = "creatediv";
+            newDiv.className = 'card_div'
+
+            newDiv.innerHTML = `
             <div id="title-div">
                 <h3>${entris[count].title}</h3>
                 <h6>${entris[count].message}</h6>
@@ -146,12 +137,14 @@ function rearrangeDivs() {
                 <button type="button" id="delete" class="btn btn-success" onclick="datadelete(${count + 1})">Delete</button>
                 <button type="button" id="edit" class="btn btn-primary" data-bs-toggle="modal"
                 data-bs-target="#editmodal" onclick="editindex(${count + 1})">Edit</button>
-                <button type="button" id="mark" class="btn btn-danger" onclick="read(${count + 1})">Mark us complete</button>
+                <button type="button" id="mark" class="btn btn-danger" onclick="read(${count + 1})">Mark as complete</button>
             </div>
         `;
 
-        // Append the new div to the container
-        container.appendChild(newDiv);
+            // Append the new div to the container
+            container.appendChild(newDiv);
+        }
+
         count++
     }
 
